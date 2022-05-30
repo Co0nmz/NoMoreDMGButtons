7e35b84af80ef3e52b8fa67ad9ec4a8115c81af3
Co0nmz <99102789+Co0nmz@users.noreply.github.com>
2022-05-30 22:50:28 +0200

    Update NoMoreDMGButtons.mjs

--- NoMoreDMGButtons.mjs
+++ NoMoreDMGButtons.mjs
@@ -1,4 +1,4 @@
 Hooks.on('renderChatMessage', (_, jq) => {
   if (game.user.isGM) return;
-  jq.find('[data-action="applyDamage"]').closest('.card-buttons').remove();
+  jq.find('[data-action="damage"]').closest('.card-buttons').remove();
 });
